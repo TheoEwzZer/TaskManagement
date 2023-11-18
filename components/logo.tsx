0ea2479985290e89f124c621b20d5f2283a83b89
Théo <theo.fabianomattei@gmail.com>
2023-11-19 00:49:02 +0100

    feat: add Logo and Button components

---
+++ components/logo.tsx
@@ -0,0 +1,23 @@
+import { ReactElement } from "react";
+
+import Link from "next/link";
+import localFont from "next/font/local";
+import { NextFont } from "next/dist/compiled/@next/font";
+
+import { cn } from "@/lib/utils";
+
+const headingFont: NextFont = localFont({
+  src: "../public/fonts/font.woff2",
+});
+
+export function Logo(): ReactElement {
+  return (
+    <Link href="/">
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <p className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}>
+          TaskManagement
+        </p>
+      </div>
+    </Link>
+  );
+}
