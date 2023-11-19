2d1a358635df8f4b2a09a17d5b466c0d0995d35f
Théo <theo.fabianomattei@gmail.com>
2023-11-19 13:36:50 +0100

    feat: add OrganizationIdPage component

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+function OrganizationIdPage(): ReactElement {
+  return (
+    <div className="w-full mb-20">
+      <div className="px-2 md:px-4">OrganizationIdPage</div>
+    </div>
+  );
+}
+
+export default OrganizationIdPage;
