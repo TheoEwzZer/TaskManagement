f1f8260588de7af2b55c095cf41ba7012d8ea350
Théo <theo.fabianomattei@gmail.com>
2023-11-19 13:35:53 +0100

    security: add authMiddleware changes to middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,40 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { AuthObject } from "@clerk/backend";
+
+import { NextRequest, NextResponse } from "next/server";
+
+type Auth = AuthObject & {
+  isPublicRoute: boolean;
+  isApiRoute: boolean;
+};
+
+type WithClerkUrl<T> = T & {
+  experimental_clerkUrl: NextRequest["nextUrl"];
+};
 
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth(auth: Auth, req: WithClerkUrl<NextRequest>): any {
+    if (auth.userId && auth.isPublicRoute) {
+      let path: string = "select-org";
+
+      if (auth.orgId) {
+        path = `/organization/${auth.orgId}`;
+      }
+
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
 });
 
 export const config = {
