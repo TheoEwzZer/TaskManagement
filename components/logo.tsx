4854f813ce10ad2710a1965a7cf5242f0a754346
Théo <theo.fabianomattei@gmail.com>
2023-11-21 22:56:11 +0100

    style: add logo image

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,8 +1,9 @@
 import { ReactElement } from "react";
 
-import Link from "next/link";
-import localFont from "next/font/local";
 import { NextFont } from "next/dist/compiled/@next/font";
+import localFont from "next/font/local";
+import Image from "next/image";
+import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 
@@ -14,6 +15,12 @@ export function Logo(): ReactElement {
   return (
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          height={40}
+          width={40}
+        />
         <p className={cn("text-lg text-neutral-700", headingFont.className)}>
           TaskManagement
         </p>
