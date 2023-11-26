ba0ac8820b03f236ebe80aa984a997a960473eda
Théo <theo.fabianomattei@gmail.com>
2023-11-26 14:19:35 +0100

    feat: update navbar component to include form popover and new button

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,10 +1,12 @@
 import { ReactElement } from "react";
 
-import { PlusIcon } from "lucide-react";
+import { Plus, PlusIcon } from "lucide-react";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
+import { FormPopover } from "@/components/form/form-popover";
+
 import { MobileSidebar } from "./mobile-sidebar";
 
 function Navbar(): ReactElement {
@@ -15,20 +17,28 @@ function Navbar(): ReactElement {
         <div className="hidden md:flex">
           <Logo />
         </div>
-        <Button
-          variant="primary"
-          size="sm"
-          className="rounded-sm hidden md:block h-auto py-1.5 px-2"
+        <FormPopover
+          align="start"
+          side="bottom"
+          sideOffset={18}
         >
-          Create
-        </Button>
-        <Button
-          variant="primary"
-          size="sm"
-          className="rounded-sm block md:hidden 2"
-        >
-          <PlusIcon className="h-4 w-4" />
-        </Button>
+          <Button
+            variant="primary"
+            size="sm"
+            className="rounded-sm hidden md:block h-auto py-1.5 px-2"
+          >
+            Create
+          </Button>
+        </FormPopover>
+        <FormPopover>
+          <Button
+            variant="primary"
+            size="sm"
+            className="rounded-sm block md:hidden"
+          >
+            <Plus className="h-4 w-4" />
+          </Button>
+        </FormPopover>
       </div>
       <div className="ml-auto flex items-center gap-x-2">
         <OrganizationSwitcher
