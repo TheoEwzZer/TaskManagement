b95e929648f4fb2ad29db33a056fa26826c7453f
Théo <theo.fabianomattei@gmail.com>
2023-11-26 13:11:02 +0100

    feat: add tooltip component for hints

---
+++ components/hints.tsx
@@ -0,0 +1,37 @@
+import { ReactElement, ReactNode } from "react";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface HintProps {
+  children: ReactNode;
+  description: string;
+  side?: "left" | "right" | "top" | "bottom";
+  sideOffset?: number;
+}
+
+export function Hint({
+  children,
+  description,
+  side,
+  sideOffset,
+}: HintProps): ReactElement {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent
+          sideOffset={sideOffset}
+          side={side}
+          className="text-xs max-w-[220px] break-words"
+        >
+          {description}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
