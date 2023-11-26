8116937c15106ec6c598535ea46199d08e79769c
Théo <theo.fabianomattei@gmail.com>
2023-11-26 13:11:59 +0100

    feat: add Toaster component to PlatformLayout

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,9 +1,15 @@
 import { ReactElement, ReactNode } from "react";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 function PlatformLayout({ children }: { children: ReactNode }): ReactElement {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return (
+    <ClerkProvider>
+      <Toaster />
+      {children}
+    </ClerkProvider>
+  );
 }
 
 export default PlatformLayout;
