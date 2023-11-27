991335a216a98508e08fc522f7c0768b19db4e02
Maestracci Théo <theomaestracci@gmail.com>
2023-11-27 22:03:16 +0100

    style: complete layout for the home page

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -5,13 +5,13 @@ import { Button } from "@/components/ui/button";
 
 function Footer(): ReactElement {
   return (
-    <div className="fixed bottom-0 w-full p-4 bg-white">
+    <div className="w-full p-4 border-t bg-slate-100">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
           <Button
             size="sm"
-            className="text-white bg-orange-600 hover:bg-orange-700"
+            className="text-white bg-black hover:"
           >
             Privacy Policy
           </Button>
--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,49 +2,18 @@ import { ReactElement, ReactNode } from "react";
 
 import Navbar from "./_components/navbar";
 import Footer from "./_components/footer";
+import Features from "./_components/features";
+import Testimonials from "./_components/testimonials";
 
 function MarketingLayout({ children }: { children: ReactNode }): ReactElement {
   return (
     <div className="h-full">
       <Navbar />
-      <main className="pt-40 pb-20 bg-gradient-to-br from-pink-600 to-yellow-400">
+      <main className="pt-40 pb-40 bg-gradient-to-br from-pink-600 to-yellow-400">
         {children}
       </main>
-      <section className="bg-white pt-20 pb-40">
-        <div className="container mx-auto">
-          <h2 className="text-3xl font-bold mb-8 text-center">Our Features</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-            <div className="bg-gray-200 p-6 rounded-lg shadow-xl">
-              <h3 className="text-lg font-semibold mb-2">Feature 1</h3>
-              <p className="text-sm text-gray-600">
-                Description of Feature 1 lorem ipsum dolor sit amet consectetur
-                adipisicing elit.
-              </p>
-            </div>
-            <div className="bg-gray-200 p-6 rounded-lg shadow-xl">
-              <h3 className="text-lg font-semibold mb-2">Feature 2</h3>
-              <p className="text-sm text-gray-600">
-                Description of Feature 2 lorem ipsum dolor sit amet consectetur
-                adipisicing elit.
-              </p>
-            </div>
-            <div className="bg-gray-200 p-6 rounded-lg shadow-xl">
-              <h3 className="text-lg font-semibold mb-2">Feature 3</h3>
-              <p className="text-sm text-gray-600">
-                Description of Feature 3 lorem ipsum dolor sit amet consectetur
-                adipisicing elit.
-              </p>
-            </div>
-            <div className="bg-gray-200 p-6 rounded-lg shadow-xl">
-              <h3 className="text-lg font-semibold mb-2">Feature 4</h3>
-              <p className="text-sm text-gray-600">
-                Description of Feature 4 lorem ipsum dolor sit amet consectetur
-                adipisicing elit.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Features />
+      <Testimonials />
       <Footer />
     </div>
   );
