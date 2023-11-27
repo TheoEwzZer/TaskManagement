5f24603db7fa7e0fbeb309c85a43dbae31941056
Théo <theo.fabianomattei@gmail.com>
2023-11-27 22:37:55 +0100

    feat: refactor board/[boardId]/page.tsx to include authentication and fetch data

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,7 +1,53 @@
 import { ReactElement } from "react";
 
-function BoardIdPage(): ReactElement {
-  return <div>BoardIdPage</div>;
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+
+import { ListContainer } from "./_components/list-container";
+import { ListWithCards } from "@/types";
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+
+async function BoardIdPage({ params }: BoardIdPageProps): Promise<ReactElement> {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists: ListWithCards[] = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer
+        boardId={params.boardId}
+        data={lists}
+      />
+    </div>
+  );
 }
 
 export default BoardIdPage;
