b7b6fe8654bed7d91204e3902e0782de9496182a
Théo <theo.fabianomattei@gmail.com>
2023-11-28 21:47:04 +0100

    feat: add copyList action to copy a list with cards

---
+++ actions/copy-list/index.ts
@@ -0,0 +1,100 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+
+import { db } from "@/lib/db";
+
+import { ActionState, createSafeAction } from "@/lib/create-safe-action";
+import { ListWithCards } from "@/types";
+import { Card, List } from "@prisma/client";
+import { CopyList } from "./schema";
+import { InputType, ReturnType } from "./types";
+
+async function handler(data: InputType): Promise<ReturnType> {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { id, boardId } = data;
+
+  let list: List;
+
+  try {
+    const listToCopy: ListWithCards | null = await db.list.findUnique({
+      where: {
+        id,
+        boardId,
+        board: {
+          orgId,
+        },
+      },
+      include: {
+        cards: true,
+      },
+    });
+
+    if (!listToCopy) {
+      return {
+        error: "List not found.",
+      };
+    }
+
+    const lastList: { order: number } | null = await db.list.findFirst({
+      where: {
+        boardId,
+      },
+      orderBy: {
+        order: "desc",
+      },
+      select: {
+        order: true,
+      },
+    });
+
+    const newOrder: number = lastList ? lastList.order + 1 : 1;
+
+    list = await db.list.create({
+      data: {
+        boardId: listToCopy.boardId,
+        title: `${listToCopy.title} - Copy`,
+        order: newOrder,
+        cards: {
+          createMany: {
+            data: listToCopy.cards.map(
+              (
+                card: Card
+              ): { title: string; order: number; description: string | null } => ({
+                title: card.title,
+                order: card.order,
+                description: card.description,
+              })
+            ),
+          },
+        },
+      },
+      include: {
+        cards: true,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to create.",
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: list };
+}
+
+export const copyList: (data: {
+  id: string;
+  boardId: string;
+}) => Promise<ActionState<{ id: string; boardId: string }, List>> = createSafeAction(
+  CopyList,
+  handler
+);
