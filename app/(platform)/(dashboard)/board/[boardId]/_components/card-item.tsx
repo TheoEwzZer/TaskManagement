f5748e996849d9c631cb5ea4ebbccfac4a5cfb97
Théo <theo.fabianomattei@gmail.com>
2023-11-29 21:09:38 +0100

    feat: add card modal functionality to CardItem component

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from "react";
 
 import { Card } from "@prisma/client";
 import { Draggable, DraggableProvided } from "@hello-pangea/dnd";
+import { CardModalStore, useCardModal } from "@/hooks/use-card-modal";
 
 interface CardItemProps {
   data: Card;
@@ -9,6 +10,8 @@ interface CardItemProps {
 }
 
 export function CardItem({ data, index }: CardItemProps): ReactElement {
+  const cardModal: CardModalStore = useCardModal();
+
   return (
     <Draggable
       draggableId={data.id}
@@ -20,6 +23,7 @@ export function CardItem({ data, index }: CardItemProps): ReactElement {
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           role="button"
+          onClick={(): void => cardModal.onOpen(data.id)}
           className="truncate border-2 border-transparent hover:border-black shadow-sm py-2 px-3 text-sm bg-white rounded-md"
         >
           {data.title}
