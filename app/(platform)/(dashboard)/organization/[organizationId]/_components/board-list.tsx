58d7adc70f3c7496619c6258aa024755fb77458c
Théo <theo.fabianomattei@gmail.com>
2023-12-01 22:09:58 +0100

    feat: add available board count to create new board button

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -10,6 +10,8 @@ import { Board } from "@prisma/client";
 import { Hint } from "@/components/hint";
 import { Skeleton } from "@/components/ui/skeleton";
 import { FormPopover } from "@/components/form/form-popover";
+import { getAvailableCount } from "@/lib/org-limit";
+import { MAX_FREE_BOARDS } from "@/constants/boards";
 
 export async function BoardList(): Promise<ReactElement> {
   const { orgId } = auth();
@@ -26,6 +28,8 @@ export async function BoardList(): Promise<ReactElement> {
       createdAt: "desc",
     },
   });
+
+  const availableCount: number = await getAvailableCount();
 
   return (
     <div className="space-y-4">
@@ -56,7 +60,9 @@ export async function BoardList(): Promise<ReactElement> {
             className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
           >
             <p className="text-sm">Create new board</p>
-            <span className="text-xs">5 remaining</span>
+            <span className="text-xs">
+              {`${MAX_FREE_BOARDS - availableCount} remaining`}
+            </span>
             <Hint
               sideOffset={40}
               side="bottom"
