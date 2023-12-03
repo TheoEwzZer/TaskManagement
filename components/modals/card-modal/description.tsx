190a0fa6b6fdd219f06a3c5f839262786c6ce06a
Théo <theo.fabianomattei@gmail.com>
2023-12-03 23:53:56 +0100

    style: increase height of description container

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -117,7 +117,7 @@ export function Description({ data }: DescriptionProps): ReactElement {
           <div
             onClick={enableEditing}
             role="button"
-            className="min-h-[78px] bg-neutral-200 text-sm font-medium py-3 px-3.5 rounded-md"
+            className="min-h-[112px] bg-neutral-200 text-sm font-medium py-3 px-3.5 rounded-md"
           >
             {data.description || "Add a more detailed description..."}
           </div>
