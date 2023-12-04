a76e27bc26dece5bace2252e239cb69380b6c372
Théo <theo.fabianomattei@gmail.com>
2023-12-04 19:34:21 +0100

    feat: add Avatar component to CardItem

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -3,6 +3,7 @@ import { ReactElement } from "react";
 import { Card } from "@prisma/client";
 import { Draggable, DraggableProvided } from "@hello-pangea/dnd";
 import { CardModalStore, useCardModal } from "@/hooks/use-card-modal";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 interface CardItemProps {
   data: Card;
@@ -24,9 +25,17 @@ export function CardItem({ data, index }: CardItemProps): ReactElement {
           ref={provided.innerRef}
           role="button"
           onClick={(): void => cardModal.onOpen(data.id)}
-          className="truncate border-2 border-transparent hover:border-black shadow-sm py-2 px-3 text-sm bg-white rounded-md"
+          className="border-2 border-transparent hover:border-black shadow-sm py-2 px-3 text-sm bg-white rounded-md flex justify-between"
         >
-          {data.title}
+          <div>{data.title}</div>
+          {data && data.userImage && data.userName && (
+            <Avatar className="h-6 w-6">
+              <AvatarImage
+                src={data.userImage}
+                alt={data.userName}
+              />
+            </Avatar>
+          )}
         </div>
       )}
     </Draggable>
