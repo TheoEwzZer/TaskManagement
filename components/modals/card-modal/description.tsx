ce7dacd6e753310cefae757f99926ecb79deea6d
Théo <theo.fabianomattei@gmail.com>
2023-12-04 21:34:16 +0100

    style: update card-modal description styling

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -132,7 +132,7 @@ Description.Skeleton = function DescriptionSkeleton(): ReactElement {
     <div className="flex items-start gap-x-3 w-full">
       <Skeleton className="h-6 w-6 bg-neutral-200" />
       <div className="w-full">
-        <Skeleton className="h-6 w-24 bg-neutral-200" />
+        <Skeleton className="h-6 w-24 mb-2 bg-neutral-200" />
         <Skeleton className="h-20 w-full bg-neutral-200" />
       </div>
     </div>
