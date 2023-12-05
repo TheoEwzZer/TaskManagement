12cbeb364eb2ed7445472e8460cecf7667bd5938
Théo <theo.fabianomattei@gmail.com>
2023-12-05 18:26:22 +0100

    style: update board-list grid layout

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -39,7 +39,7 @@ export async function BoardList(): Promise<ReactElement> {
         <User2 className="h-6 w-6 mr-2" />
         Your boards
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {boards.map(
           (board: Board): ReactElement => (
             <Link
@@ -81,7 +81,7 @@ export async function BoardList(): Promise<ReactElement> {
 
 BoardList.Skeleton = function BoardListSkeleton(): ReactElement {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
       <Skeleton className="aspect-video h-full w-full p-2" />
       <Skeleton className="aspect-video h-full w-full p-2" />
       <Skeleton className="aspect-video h-full w-full p-2" />
