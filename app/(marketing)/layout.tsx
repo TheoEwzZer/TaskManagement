99997349d0f508bf3a19b87fc9fb559441f9e182
Maestracci Théo <theomaestracci@gmail.com>
2023-12-07 22:09:03 +0100

    style: add css for home page and board page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,18 +2,12 @@ import { ReactElement, ReactNode } from "react";
 
 import Navbar from "./_components/navbar";
 import Footer from "./_components/footer";
-import Features from "./_components/features";
-import Testimonials from "./_components/testimonials";
 
 function MarketingLayout({ children }: { children: ReactNode }): ReactElement {
   return (
     <div className="h-full">
       <Navbar />
-      <main className="pt-40 pb-40 bg-gradient-to-br from-pink-600 to-yellow-400">
-        {children}
-      </main>
-      <Features />
-      <Testimonials />
+      <main>{children}</main>
       <Footer />
     </div>
   );
