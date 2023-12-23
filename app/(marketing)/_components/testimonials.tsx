b731ee19059e2828dc41d041faf33d0ca25aab4d
Théo <theo.fabianomattei@gmail.com>
2023-12-23 17:47:53 +0100

    style: improve carousel style

--- app/(marketing)/_components/testimonials.tsx
+++ app/(marketing)/_components/testimonials.tsx
@@ -1,47 +1,25 @@
 "use client";
 
 import { ReactElement } from "react";
+import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures";
+import Autoplay from "embla-carousel-autoplay";
 
-import { motion } from "framer-motion";
-import { Testimony, useTestimonial } from "@/hooks/use-testimonial";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
-import { Button } from "@/components/ui/button";
-
-const FORWARD = 1;
-const BACKWARD = -1;
-
-type TestimonialProps = {
-  testimonial: Testimony;
-  direction: typeof FORWARD | typeof BACKWARD;
-};
-
-function Testimonial({ testimonial, direction }: TestimonialProps): ReactElement {
-  return (
-    <motion.div
-      className="testimonial-item"
-      initial={
-        direction === FORWARD ? { opacity: 0, x: "100%" } : { opacity: 0, x: "-100%" }
-      }
-      animate={{ opacity: 1, x: 0 }}
-      exit={
-        direction === FORWARD ? { opacity: 0, x: "-100%" } : { opacity: 0, x: "100%" }
-      }
-      transition={{ duration: 0.5 }}
-      key={testimonial.id}
-    >
-      <div>
-        <p className="mb-2 text-lg font-semibold text-gray-700">{testimonial.name}</p>
-        <p className="text-sm text-gray-600">{testimonial.comment}</p>
-      </div>
-    </motion.div>
-  );
+export interface Testimony {
+  name: string;
+  comment: string;
 }
-
 function Testimonials(): ReactElement {
   const testimonies: Testimony[] = [
     {
-      id: 1,
       name: "John Doe",
       comment: `
         TaskManagement is fantastic! 
@@ -50,21 +28,18 @@ function Testimonials(): ReactElement {
       `,
     },
     {
-      id: 2,
       name: "Jane Smith",
       comment: `
       I've found TaskManagement incredibly useful.
       It's user-friendly and has boosted our team's productivity significantly.`,
     },
     {
-      id: 3,
       name: "Emily Johnson",
       comment: `
       As a freelancer, TaskManagement keeps me on track with its easy-to-use features.
       Highly recommended for personal use!`,
     },
     {
-      id: 4,
       name: "Michael Brown",
       comment: `
       TaskManagement has become an essential tool for our team.
@@ -72,46 +47,54 @@ function Testimonials(): ReactElement {
     },
   ];
 
-  const { currentIndex, direction, nextTestimonial, prevTestimonial } =
-    useTestimonial(testimonies);
-
   return (
     <div className="bg-white py-12">
       <h2 className="mb-8 text-center text-4xl font-extrabold text-orange-600">
         Testimonials
       </h2>
-      <div className="mb-4 flex items-center justify-center">
-        <Button
-          variant="link"
-          onClick={prevTestimonial}
-          className="mr-4"
+      <div className="flex w-full items-center justify-center">
+        <Carousel
+          className="w-full max-w-screen-md"
+          opts={{ loop: true }}
+          plugins={[WheelGesturesPlugin(), Autoplay({ delay: 4000 })]}
         >
-          <ChevronLeft className="h-10 w-10" />
-        </Button>
-        <motion.div
-          className={`
-            testimonial-container
-            relative rounded-xl 
-            bg-gray-200 
-            p-8 
-            font-bold 
-            shadow-xl 
-            ${"sm:w-full md:w-3/4 lg:w-2/3 xl:w-1/2"}
-            ${"overflow-hidden sm:max-h-96 md:max-h-80 lg:max-h-64 xl:max-h-56"}
-          `}
-        >
-          <Testimonial
-            testimonial={testimonies[currentIndex]}
-            direction={direction}
-          />
-        </motion.div>
-        <Button
-          variant="link"
-          onClick={nextTestimonial}
-          className="ml-4"
-        >
-          <ChevronRight className="h-10 w-10" />
-        </Button>
+          <CarouselContent>
+            {testimonies.map(
+              (testimony: Testimony, index: number): ReactElement => (
+                <CarouselItem key={index}>
+                  <div className="px-8">
+                    <Card
+                      className="
+                        testimonial-container
+                        relative overflow-hidden 
+                        rounded-xl 
+                      bg-gray-200 
+                        p-4 
+                        font-bold 
+                        sm:max-h-96 
+                        sm:w-full 
+                        md:max-h-80
+                        lg:max-h-64 
+                        xl:max-h-56
+                      "
+                    >
+                      <CardContent>
+                        <div>
+                          <p className="mb-2 text-lg font-semibold text-gray-700">
+                            {testimony.name}
+                          </p>
+                          <p className="text-sm text-gray-600">{testimony.comment}</p>
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              )
+            )}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </div>
   );
